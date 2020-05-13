1e7aa24024b77d978b4e93d0614a61e929b2c7cd
impact learner <time.darden@gmail.com>
2020-05-13 19:12:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 # your code goes here
 require 'pry'
 class Person
-attr_reader :name
-attr_accessor :bank_account, :happiness, :hygiene
+attr_reader :name, :hygiene
+attr_accessor :bank_account, :happiness
   def initialize (name)
     @name = name
     @bank_account = 25
